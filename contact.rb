0563c5bfa8a4a7e36c2bc743a0cbe4eaf17f01e3
ilia24 <iliademertchian24@gmail.com>
2017-04-07 12:00:51 -0400

    Add base header styling

--- contact.rb
+++ contact.rb
@@ -5,7 +5,7 @@ require 'mini_record'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
-class Contact <ActiveRecord::Base
+class Contact < ActiveRecord::Base
 
   field :first_name, as: :string
   field :last_name,  as: :string
